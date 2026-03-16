d779eb50d6a3b4b3d0ae90ec911dde2e2b595bfd
K. Vamsi <kvamsi1316@gmail.com>
2026-03-16 07:19:23 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,16 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        int num = 121, temp = num, rev = 0;
+
+        while(num != 0) {
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num = num / 10;
+        }
+
+        if(temp == rev)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
